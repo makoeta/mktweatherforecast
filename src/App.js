0367d423d8615d2e4141ad9bc82757fc91ff0702
Maximilian König <maximilian.koenig@mein.gmx>
2023-07-28 17:34:13 +0200

    added basic event

--- src/App.js
+++ src/App.js
@@ -2,37 +2,49 @@ import './App.css';
 
 import "leaflet/dist/leaflet.css"
 
-import {MapContainer, TileLayer} from "react-leaflet";
-
-
-function LeafletMap() {
-  return (
-    <MapContainer center={[52.520008, 13.404954]} zoom={13}>
-      <TileLayer
-        attribution={"© OpenStreetMap"}
-        url={"https://tile.openstreetmap.org/{z}/{x}/{y}.png"}
-      />
-    </MapContainer>
-  );
-}
-
-function Search() {
-  return (
-    <div>
-      <div className="text">How's the weather in...</div>
-
-      <LeafletMap/>
-
-      <div className={"text"}>?</div>
-    </div>
-  );
-}
-
-
-
-
+import {MapContainer, Marker, Popup, TileLayer, useMapEvents} from "react-leaflet";
+import {useState} from "react";
+import {MarkerProps} from "react-leaflet";
 
 function App() {
+
+
+  function GetLatLng() {
+    const [latlng, setLatlng] = useState(null);
+
+    useMapEvents({
+      click(e) {
+        setLatlng(e.latlng)
+        alert("Location set to: " + latlng)
+
+      }
+    })
+  }
+
+  function Search() {
+    function LeafletMap({lat, lng, zoom = 6}) {
+      return (
+        <MapContainer center={[lat, lng]} zoom={zoom}>
+          <TileLayer
+            attribution={"© OpenStreetMap"}
+            url={"https://tile.openstreetmap.org/{z}/{x}/{y}.png"}
+          />
+          <GetLatLng />
+        </MapContainer>
+      );
+    }
+
+    return (
+      <div>
+        <div className="text">How's the weather in...</div>
+
+        <LeafletMap lat={"51.097"} lng={"10.558"}/>
+
+        <div className={"text"}>?</div>
+      </div>
+    );
+  }
+
 
   let mode = "search"
 
