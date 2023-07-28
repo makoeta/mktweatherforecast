72d0f60eb0e3bc30cdc3121ff006a269b14a6886
Maximilian König <maximilian.koenig@mein.gmx>
2023-07-28 17:07:33 +0200

    leaflet map instead of input text field

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import './App.css';
 
+import "leaflet/dist/leaflet.css"
+
+import {MapContainer, TileLayer} from "react-leaflet";
+
+
+function LeafletMap() {
+  return (
+    <MapContainer center={[52.520008, 13.404954]} zoom={13}>
+      <TileLayer
+        attribution={"© OpenStreetMap"}
+        url={"https://tile.openstreetmap.org/{z}/{x}/{y}.png"}
+      />
+    </MapContainer>
+  );
+}
+
 function Search() {
   return (
     <div>
       <div className="text">How's the weather in...</div>
 
-      <input className="searchbar" placeholder={"Your location"}></input>
+      <LeafletMap/>
 
       <div className={"text"}>?</div>
     </div>
   );
 }
 
+
+
+
+
 function App() {
 
   let mode = "search"
-
-
 
   if (mode === "search") {
     return (<Search/>);
